6532d1e113767ecdd01042275cedd02e1da974e9
starfish01 <patrick.labes@gmail.com>
2018-03-05 06:41:45 +1000

    lesson on http requests

---
+++ S2/http-request.js
@@ -0,0 +1,38 @@
+const https = require('https');
+const fs = require('fs');
+
+const url = 'https://jsonplaceholder.typicode.com/posts';
+
+https.get(url, res =>{
+    res.setEncoding('utf8');
+    let body ='';
+
+    res.on('data', data=>{
+        body += data;
+    });
+
+    res.on('end', ()=>{
+        fs.writeFile('data.json', body, 'utf8', (err)=>{
+            if(err) return err;
+            console.log("created file")
+        });
+        
+        // body = JSON.parse(body);
+
+        // console.log(
+            
+        //     // body.forEach(element => {
+        //     //     `${element.title}`
+        //     // });
+
+        //     `
+        //     ${body[0].title}
+            
+            
+            
+        //     `
+        // )
+
+    })
+
+});
