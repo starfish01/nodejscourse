06bc1e08b0d8010c0c176916322af24a5d5953e0
starfish01 <patrick.labes@gmail.com>
2018-03-07 07:46:43 +1000

    morning work

--- S2/http-form-submit.js
+++ S2/http-form-submit.js
@@ -8,7 +8,40 @@ http.createServer((req,res)=>{
     if(req.method === 'GET'){
         console.log(req.method);
 
+        res.writeHead(200,{'Content-Type': 'text/html'});
+
+        fs.readFile('./http-form.html', 'UTF-8', (err, data)=>{
+            if(err) throw err;
+
+            res.write(data);
+            res.end();
+
+        });
+
+    }else if(req.method === 'POST'){
+
+        req.on('data', (data)=>{
+            body += data;
+        });
+        req.on('end', ()=>{
+
+            res.writeHead(200,{'Content-Type': 'text/html'});
+            res.write(body, ()=>{
+                res.end();
+            });
+
+        });
+    } else {
+
+        res.writeHead(404,{'Content-Type': 'text/html'});
+        res.end('404 ERROR could not find that page')
+
     }
+
+
+
+
+
 }).listen(4444);
 
 console.log('its working');
