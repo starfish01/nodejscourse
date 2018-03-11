1777779969a86aa0f357eec2b673c21589988725
starfish01 <patrick.labes@gmail.com>
2018-03-11 16:18:16 +1000

    inserting into db

--- s3/app.js
+++ s3/app.js
@@ -1,44 +1,37 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/animals');
-mongoose.connection
-.once('open', ()=>console.log('connected'))
-.on('error', (err)=> {
-    console.log(`Could not connect`, err)
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var MongoClient = require('mongodb').MongoClient
-
-// MongoClient.connect('mongodb://localhost:27017/animals', function (err, db) {
-//   if (err) throw err
-
-//   console.log('CONNECTED')
-
-
-// //   db.collection('mammals').find().toArray(function (err, result) {
-// //     if (err) throw err
-
-// //     console.log(result)
-// //   })
+// mongoose.connect('mongodb://localhost:27017/animals');
+// mongoose.connection
+// .once('open', ()=>console.log('connected'))
+// .on('error', (err)=> {
+//     console.log(`Could not connect`, err)
 // })
+
+
+
+
+
+var MongoClient = require('mongodb').MongoClient
+
+MongoClient.connect('mongodb://localhost:27017/', function (err, client) {
+  if (err) throw err
+
+  console.log('CONNECTED');
+
+  const db = client.db('animals');
+
+    db.collection('mammals').insertOne({
+        name: 'horse'    
+    }, (err, result)=>{
+        if(err){ return console.log(err)};
+        console.log('INSERTED');
+    });
+
+
+
+//   db.collection('mammals').find().toArray(function (err, result) {
+//     if (err) throw err
+
+//     console.log(result)
+//   })
+})
