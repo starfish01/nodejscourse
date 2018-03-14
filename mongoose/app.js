2f5d98e900c8e6efdf61fae905b341e45d5ef1b5
starfish01 <patrick.labes@gmail.com>
2018-03-14 21:22:08 +1000

    evening lessons

--- mongoose/app.js
+++ mongoose/app.js
@@ -2,6 +2,13 @@ const mongoose = require('mongoose');
 const User = require('./models/User');
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+mongoose.Promise = global.Promise;
 
 
 mongoose.connect('mongodb://localhost:27017/mongoose');
@@ -12,19 +19,52 @@ mongoose.connection
 });
 
 
-app.post('/users');
+app.get('/', (req, res)=>{
+    res.send('Root');
+});
 
 
-// const newUser = new User({
-//     firstName: 'Jack',
-//     lastName: 'Mal',
-//     isActive: 1
-// });
+app.post('/users', (req,res)=>{
+    const newUser = new User({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        isActive: req.body.isActive
+    }); 
+    
+    newUser.save().then(savedUser =>{
+        console.log('Saved User');
+    }).catch(err=>{
+        res.status(404).send(`User Not Saved ... ${err}`)
+    });
+    res.send(newUser);
+});
 
-// newUser.save(function(err, dataSaved){
-//     if(err) return err;
-//     console.log(dataSaved)
-// });
+
+app.get('/users', (req,res)=>{
+    User.find({}).then(users =>{
+        res.status(200).send(users);
+    }).catch(err=>{
+        res.status(404).send(`Data Not Found ... ${err}`)
+    });
+});
+
+
+app.patch('/users/:id',(req, res)=>{
+    
+    const id = req.params.id;
+    const firstName = req.body.firstName;
+
+    User.findByIdAndUpdate(id, {$set: {firstName: firstName}}, {new: true})
+    .then(savedUser=>{
+        res.status(200).send('Saved by patch');
+        console.log('Saved Via Patch');
+    });
+
+});
+
+//patch or put
+
+
 
 const port = 4444 || process.env.PORT;
 
