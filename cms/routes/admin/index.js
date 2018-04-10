1525c90f493d3f04b789f76fe04968e58830284c
starfish01 <patrick.labes@gmail.com>
2018-04-10 20:35:19 +1000

    Found the issue i didnt fully understand promises and asyn but now that is working its working well!

--- cms/routes/admin/index.js
+++ cms/routes/admin/index.js
@@ -4,21 +4,21 @@ const faker = require('faker');
 const Post = require('../../models/Post');
 const User = require('../../models/User');
 const Comment = require('../../models/Comment');
-const {userAuthenticated} = require('../../helpers/authentication');
+const { userAuthenticated } = require('../../helpers/authentication');
 const Categories = require('../../models/Categories');
 
-router.all('/*', userAuthenticated, (req, res, next)=>{
+router.all('/*', userAuthenticated, (req, res, next) => {
     req.app.locals.layout = 'admin';
     next();
 });
 
 
-router.get('/', (req, res)=>{
+router.get('/', (req, res) => {
 
-    Post.count({}).then(postCount=>{
-        User.count({}).then(userCount=>{
-            Comment.count({}).then(commentCount=>{
-                res.render('admin/index',{postCount: postCount, userCount:userCount, commentCount:commentCount});
+    Post.count({}).then(postCount => {
+        User.count({}).then(userCount => {
+            Comment.count({}).then(commentCount => {
+                res.render('admin/index', { postCount: postCount, userCount: userCount, commentCount: commentCount });
             })
         })
 
@@ -27,60 +27,55 @@ router.get('/', (req, res)=>{
 });
 
 
-router.post('/generate-fake-posts', (req, res)=>{
+router.post('/generate-fake-posts', (req, res) => {
 
-    let cat =[];
+
 
     Categories.find({}).then(cats=>{
-        cats.forEach(item=>{
-            cat.push(item.id);
+
+        var cat = [];
+
+        cats.forEach(cats => {
+            cat.push(cats.id)
+        
+        })
+
+        return cat;
+    
+    }).then(cat=>{
+        console.log(cat)
 
             for (let index = 0; index < req.body.numberOfLoops; index++) {
 
                 let newPost = new Post();
-                newPost.title= faker.name.title(),
-                newPost.status= faker.random.arrayElement(["public","private","draft"]),
-                newPost.allowComments= faker.random.boolean(),
-                newPost.body= faker.lorem.paragraph(),
-                newPost.category= faker.random.arrayElement(cat),
-                newPost.user=req.user.id
+                newPost.title = faker.name.title(),
+                    newPost.status = faker.random.arrayElement(["public", "private", "draft"]),
+                    newPost.allowComments = faker.random.boolean(),
+                    newPost.body = faker.lorem.paragraph(),
+                    newPost.category = faker.random.arrayElement(cat),
+                    newPost.user = req.user.id
                 newPost.slug = faker.name.title();
         
-                newPost.save(function(err){
-                    if(err) throw err;
-
-                    
-                })
+                newPost.save(function (err) {
+                    if (err) throw err;
         
+                })
             }
-        })
+            res.redirect('/admin');
+
+    }).catch(err=>{
+        console.log(err)
     })
 
-   
-
-    res.redirect('/admin');
+    
     
 
 
-        // let newPost = new Post({
-        //     title: faker.name.title(),
-        //     status: faker.random.arrayElement(["public","private","draft"]),
-        //     allowComments: faker.random.boolean(),
-        //     body: faker.lorem.paragraph(),
-        //     category: "5abd702793abac3866a2ee28",
-        //     user: req.user.id
-
-        // });
-
-        // newPost.save().then(savedPost =>{
-
-        //     res.redirect('/admin');
-        // }).catch(err =>{
-        //     res.redirect('/admin');
-        //     console.log(err);
-        // });
     
 
+
+
+    
 });
 
 
